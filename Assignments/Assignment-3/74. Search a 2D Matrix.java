56e30eec7e1316917e4ad480ae48c1c2b08d4e99
Vivek Chaudhary <vivek2020singh2020@gmail.com>
2025-09-29 17:35:23 +0530

    solution 1 added

--- Assignments/Assignment-3/74. Search a 2D Matrix.java
+++ Assignments/Assignment-3/74. Search a 2D Matrix.java
@@ -1,5 +1,39 @@
-package Assignments.Assignment-3;
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        // whether element is in the range
+        if(matrix[0][0] > target || matrix[m-1][n-1] < target){
+            return false;
+        }
 
-public class 74. Search a 2D Matrix {
-    
+        // finding the row
+        int row = -1;
+        int low = 0;
+        int high = m-1;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(matrix[mid][0] <= target){
+                row = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+
+        // finding the element in that row
+        low = 0;
+        high = n-1;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(matrix[row][mid] == target){
+                return true;
+            }else if(matrix[row][mid] > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
 }
